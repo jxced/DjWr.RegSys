7ff0b85f1bebe9f3a6a0f2143564b116f1e2fb7d
djx <jxce_d@163.com>
2017-09-14 18:58:30 +0800

    ok

---
+++ DjWr.Web/Controllers/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DjWr.Web.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.test = "test";
+            var models = new
+            {
+                name = "jx",
+                age = 28
+            };
+            return View(models);
+        }
+    }
+}
